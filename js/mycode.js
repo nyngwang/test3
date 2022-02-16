205ed34bd792f5ef2a6f6d25b04a6b355654ce6a
Ning Wang <nyngwang@gmail.com>
2022-02-16 11:43:28 +0800

    skr

---
+++ js/mycode.js
@@ -0,0 +1,30 @@
+console.log('hello world')
+
+
+
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1, 1000
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
