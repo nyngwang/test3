d1995e032a728e0083bf12b33eb71a0a91433dce
Ning Wang <nyngwang@gmail.com>
2022-02-16 13:46:37 +0800

    rotation cube

--- js/mycode.js
+++ js/mycode.js
@@ -1,4 +1,4 @@
-THREE = require('./three.js')
+console.log('aksjfksdjfk')
 
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
@@ -6,3 +6,20 @@ const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerH
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
+
+const geometry = new THREE.BoxGeometry()
+const material = new THREE.MeshBasicMaterial({
+  color: 0xa2d2ff,
+})
+
+const cube = new THREE.Mesh(geometry, material)
+scene.add(cube)
+camera.position.z = 6
+
+function animate() {
+  requestAnimationFrame(animate)
+  renderer.render(scene, camera)
+  // cube.rotation.x += 0.05
+  cube.rotation.y += 0.05
+}
+animate()
