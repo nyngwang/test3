ae873ef949f54060b9ab1a43790b6041d36dd77a
Ning Wang <nyngwang@gmail.com>
2022-02-18 14:07:06 +0800

    responsive

--- src/app.js
+++ src/app.js
@@ -23,6 +23,13 @@ function animate() {
 }
 animate()
 
+window.addEventListener('resize', () => {
+  renderer.setSize(window.innerWidth, window.innerHeight)
+  camera.aspect = window.innerWidth / window.innerHeight
+
+  camera.updateProjectionMatrix()
+})
+
 // webpack hot module replacement
 if (module.hot) {
   module.hot.accept()
