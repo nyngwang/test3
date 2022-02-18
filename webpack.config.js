7cebee1db98306f79446116dc4026ff562e42e46
Ning Wang <nyngwang@gmail.com>
2022-02-18 12:55:54 +0800

    use formal name `npm run dev/build`, so can omit `mode` in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 const path = require('path')
 
 module.exports = {
-  mode: 'development', // 用 src map 來知道問題來源在哪
+  // 跑 webpack-dev-server 預設會是 `'development'`
+  // mode: 'development', // 用 src map 來知道問題來源在哪
   entry: {
     main: path.resolve(__dirname, 'src/app.js'), // `__dirname` is the current directory.
   },
