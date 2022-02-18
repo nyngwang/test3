b5cc6dd4c5126e3c70e958c8f809cade4bd6af18
Ning Wang <nyngwang@gmail.com>
2022-02-18 10:42:20 +0800

    setup webpack: move files into `dist/`, update package.json

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path')
 module.exports = {
   mode: 'development', // 用 src map 來知道問題來源在哪
   entry: {
-    main: path.resolve(__dirname, './src/mycode.js'), // `__dirname` is the current directory.
+    main: path.resolve(__dirname, 'src/app.js'), // `__dirname` is the current directory.
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
