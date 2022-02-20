cf86017beaccee1eed5e7997b1fc2376049e934a
Ning Wang <nyngwang@gmail.com>
2022-02-20 17:16:09 +0800

    three: pre-create canvas for correct ordering in `<body>`

--- src/js/app.js
+++ src/js/app.js
@@ -3,9 +3,8 @@ import style from '../main.css'
 
 console.log('hello world')
 
-const renderer = new THREE.WebGLRenderer()
+const renderer = new THREE.WebGLRenderer({ canvas: myCanvas })
 renderer.setSize(window.innerWidth, window.innerHeight)
-document.body.appendChild(renderer.domElement)
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 camera.position.z = 3
